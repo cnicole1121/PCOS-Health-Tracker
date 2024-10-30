67beaf53fdccd1fd4f61b69a6209e02fc70ff5a1
cnicole1121 <cnicole1121@gmail.com>
2024-10-29 22:11:33 -0500

    Initial commit

---
+++ pcos_health_tracker/src/main/java/com/example/pcoshealthtracker/controller/PcosHealthTrackerApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.pcoshealthtracker.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcosHealthTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcosHealthTrackerApplication.class, args);
+    }
+}
---
+++ pcos_health_tracker/src/main/java/com/example/pcoshealthtracker/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.example.pcoshealthtracker.controller;
